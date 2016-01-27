17a0536acada148458d47f5c22184dce7780abfa
dabit3 <dabit3@gmail.com>
2016-01-26 19:27:37 -0600

    changed name of state

--- Todo/app/actions/todoActions.js
+++ Todo/app/actions/todoActions.js
@@ -3,27 +3,27 @@ import * as types from './actionTypes';
 export function addTodo(name) {
 	return {
 		type: types.ADDTODO,
-		name: name
+		name
 	}
 }
 
 export function deleteTodo(id) {
 	return {
 		type: types.DELETETODO,
-		id: id
+		id
 	}
 }
 
 export function todoDone(id) {
 	return {
 		type: types.TODODONE,
-		id: id
+		id
 	}
 }
 
 export function setFilter(filter) {
 	return {
 		type: types.SETFILTER,
-		filter: filter
+		filter
 	}
 }
